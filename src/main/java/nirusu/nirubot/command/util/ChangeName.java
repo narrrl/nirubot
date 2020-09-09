1ea1dc7a230312145dc9dfa1fe552645753b7dee
Nils Pukropp <nils.pukropp@outlook.de>
2020-09-09 06:42:48 +0200

    added change nickmane command

---
+++ src/main/java/nirusu/nirubot/command/util/ChangeName.java
@@ -0,0 +1,48 @@
+package nirusu.nirubot.command.util;
+
+import java.util.List;
+
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import nirusu.nirubot.Nirubot;
+import nirusu.nirubot.command.CommandContext;
+import nirusu.nirubot.command.ICommand;
+import nirusu.nirubot.core.GuildManager;
+
+public class ChangeName implements ICommand {
+
+    @Override
+    public void execute(CommandContext ctx) {
+        if (Nirubot.isOwner(ctx.getAuthor().getIdLong())) {
+            List<String> args = ctx.getArgs();
+
+            if (args.size() < 2) {
+                return;
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 1; i < args.size(); i++) {
+                builder.append(args.get(i)).append(" ");
+            }
+
+            String newName = builder.substring(0, builder.length() - 1);
+
+            if (newName.length() > 32 || newName.length() < 2) {
+                ctx.reply("Nicknames can't be less than 2 and more than 32");
+                return;
+            }
+
+            ctx.getJDA().getSelfUser().getManager().setName(newName).complete();
+            ctx.reply("Updated name");
+
+        } else {
+            ctx.reply("Not enough permissions");
+        }
+    }
+
+    @Override
+    public MessageEmbed helpMessage(GuildManager gm) {
+        return ICommand.createHelp("Change the name of the bot user", gm.prefix(), getKey());
+    }
+    
+}
