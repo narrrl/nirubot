d50743ae62f9c8797d70b0cbe14ec2993796a945
Nils Pukropp <nils.pukropp@outlook.de>
2020-09-09 07:00:20 +0200

    added server info command

---
+++ src/main/java/nirusu/nirubot/command/util/ServerInfo.java
@@ -0,0 +1,40 @@
+package nirusu.nirubot.command.util;
+
+import java.time.format.DateTimeFormatter;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import nirusu.nirubot.Nirubot;
+import nirusu.nirubot.command.CommandContext;
+import nirusu.nirubot.command.ICommand;
+import nirusu.nirubot.core.GuildManager;
+
+public class ServerInfo implements ICommand {
+
+    @Override
+    public void execute(CommandContext ctx) {
+
+        if (ctx.getArgs().size() != 1) {
+            return;
+        }
+
+        Guild g = ctx.getGuild();
+
+        EmbedBuilder emb = new EmbedBuilder();
+
+        emb.setTitle(g.getName()).setDescription("Name: " + g.getName() + "\nCreation Date: " 
+            + g.getTimeCreated().format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm")) + "\nMember Count: " 
+            + g.getMemberCount() + "\nRegion: " + g.getRegionRaw() + "\nOwner: " 
+            + g.getOwner().getAsMention()).setThumbnail(g.getIconUrl()).setColor(Nirubot.getColor());
+        ctx.reply(emb.build());
+
+    }
+
+    @Override
+    public MessageEmbed helpMessage(GuildManager gm) {
+        return ICommand.createHelp("Lists some usefull info about the server", gm.prefix(), getKey());
+    }
+
+    
+}
