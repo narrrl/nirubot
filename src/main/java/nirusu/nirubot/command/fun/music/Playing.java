c60c6fd4ecc9df723f7cd8f26ffb351709149cb2
Nils Pukropp <nils.pukropp@outlook.de>
2020-10-06 02:24:31 +0200

    nicer playing command

--- src/main/java/nirusu/nirubot/command/fun/music/Playing.java
+++ src/main/java/nirusu/nirubot/command/fun/music/Playing.java
@@ -34,10 +34,75 @@ public final class Playing implements ICommand {
 
         EmbedBuilder emb = new EmbedBuilder();
         AudioTrackInfo info = track.getInfo();
+        String uri = info.uri.startsWith("https://www.youtube.com/watch?v=") 
+            ? "https://youtu.be/" + info.identifier +  "?t=" + (track.getPosition() / 1000) : info.uri;
+        StringBuilder progress = new StringBuilder();
+        float percent = track.getPosition() / (float) info.length;
+        int totalTiles = 10;
+        int tilesPlayed = (int) (totalTiles * percent);
+        tilesPlayed = tilesPlayed == 0 ? 1 : tilesPlayed;
+        long minutes = track.getPosition() / 1000 / 60;
+        long seconds = track.getPosition() / 1000 % 60;
+        progress.append("◄");
+        for (int i = 0; i < totalTiles; i++) {
+            if (i == tilesPlayed - 1) {
+                progress.append("🔘");
+            } else {
+                progress.append("▬");
+            }
+        }
+        progress.append("►\n " + formatTime(minutes, seconds)+ " / ");
+        minutes = track.getDuration() / 1000 / 60;
+        seconds = track.getDuration() / 1000 % 60;
+        progress.append(formatTime(minutes, seconds));
         emb.setColor(Nirubot.getColor()).setThumbnail(ctx.getGuild().getIconUrl()).setTitle("Now playing:")
-                .setDescription("[" + info.title + "]" + "(" + info.uri + ")");
+                .setDescription("[" + info.title + "]" + "(" + uri + ")\n" + progress.toString());
         ctx.reply(emb.build());
 
+    }
+
+    private String formatTime(final long minutes , final long seconds) {
+        StringBuilder out = new StringBuilder();
+
+        if (minutes > 60) {
+
+            long hours = minutes / 60;
+
+            if (hours < 10) {
+                out.append("0" + hours);
+            } else {
+                out.append(hours);
+            }
+
+            out.append(":");
+
+            long min = minutes % 60;
+
+            if (min < 10) {
+                out.append("0" + min);
+            } else {
+                out.append(min);
+            }
+        } else {
+            if (minutes == 0) {
+                out.append("0");
+            }
+            else if (minutes < 10) {
+                out.append("0" + minutes);
+            } else {
+                out.append(minutes);
+            }
+        }
+
+        out.append(":");
+
+        if (seconds < 10) {
+            out.append("0" + seconds);
+        } else {
+            out.append(seconds);
+        }
+
+        return out.toString();
     }
 
     @Override
