890927df3f05b74aa38b9dc16ba455d45d0f7e19
Nils Pukropp <nils.pukropp@outlook.de>
2020-11-16 23:53:33 +0100

    added some ideas

--- src/main/java/nirusu/nirubot/command/CommandHandler.java
+++ src/main/java/nirusu/nirubot/command/CommandHandler.java
@@ -1,9 +1,15 @@
 package nirusu.nirubot.command;
 
+import javax.annotation.Nonnull;
+
 public class CommandHandler {
 
     private CommandHandler() {
         throw new IllegalAccessError();
+    }
+
+    public static void invoke(@Nonnull CommandContext ctx,@Nonnull String key) {
+        //TODO: implement
     }
     
 }
--- src/main/java/nirusu/nirubot/listener/DiscordListener.java
+++ src/main/java/nirusu/nirubot/listener/DiscordListener.java
@@ -1,5 +1,8 @@
 package nirusu.nirubot.listener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 
@@ -7,12 +10,16 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import nirusu.nirubot.Nirubot;
+import nirusu.nirubot.annotation.Command;
+import nirusu.nirubot.command.CommandContext;
+import nirusu.nirubot.command.CommandHandler;
 import nirusu.nirubot.core.Config;
 import nirusu.nirubot.core.DiscordUtil;
 import nirusu.nirubot.core.GuildManager;
@@ -45,7 +52,14 @@ public class DiscordListener extends ListenerAdapter implements NiruListener {
 
         if (raw.startsWith(gm.prefix()) && raw.length() > gm.prefix().length()) {
             String content = raw.substring(gm.prefix().length());
+            // key for the command
+            String key = content.split("\\s+")[0];
+            // args seperated by whitespaces
+            List<String> args = List.of(content.substring(key.length()).split("\\s+"));
             // created new CommandContext with arguments as list (seperated by whitespaces)
+            CommandContext ctx = new CommandContext(new MessageReceivedEvent(event.getJDA(), event.getResponseNumber(), event.getMessage()), Command.Context.GUILD);
+            ctx.setArgs(args);
+            CommandHandler.invoke(ctx, key);
         }
     }
 
