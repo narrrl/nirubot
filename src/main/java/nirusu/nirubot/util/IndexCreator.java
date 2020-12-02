4388e05f16f3aa11f101e62bd172d4169e7f6f57
Nils Pukropp <nils.pukropp@outlook.de>
2020-12-02 05:12:23 +0100

    better createIndex method

--- src/main/java/nirusu/nirubot/util/IndexCreator.java
+++ src/main/java/nirusu/nirubot/util/IndexCreator.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import nirusu.nirubot.Nirubot;
+
 public final class IndexCreator {
-    private static final String FIRST_HALF = "<!DOCTYPE html><html lang=\"en-US\"><head profile=\"https://nirusu99.de/\">"
+    private static final String FIRST_HALF = "<!DOCTYPE html><html><head profile=\"https://nirusu99.de/\"><meta charset=\"utf-16\">"
     + "<link rel=\"icon\" type=\"image/png\" href=\"/img/favicon.png\"><link rel=\"stylesheet\" href=\"/css/style.css\"><html>"
     + "<head><title>Home - Nirusu99.de</title></head><body><div class=\"topnav\"><a class=\"active\" href=\"/\">Home</a>"
     + "<a href=\"/videos\">Videos</a><a href=\"http://nirusu99.de:8123/\">Minecraft Server Map</a><a href=\"/uni\">Uni-Material</a>"
@@ -24,7 +26,7 @@ public final class IndexCreator {
         
         index.createNewFile();
 
-        String content = FIRST_HALF + "<ul style=\"line-height:1.5\">";
+        StringBuilder content = new StringBuilder().append(FIRST_HALF + "<ul style=\"line-height:1.5\">");
 
         File zip = null;
 
@@ -32,22 +34,24 @@ public final class IndexCreator {
             if (f.getName().endsWith(".zip")) {
                 zip = f;
             } else if (!f.getName().endsWith(".html")) {
-                content += "<li><a href=\"./" + f.getName() + "\">" + f.getName() + "</a></li>";
+                content.append("<li><a href=\"./" + f.getName() + "\">" + f.getName() + "</a></li>");
             }
         }
 
-        content += "</ul>";
+        content.append("</ul>");
 
         if (zip != null) {
-            content += "<a href=\"./" + zip.getName() + "\">Download all!</a>";
+            content.append("<a href=\"./" + zip.getName() + "\">Download all!</a>");
         }
 
-        content += SECOND_HALF;
+        content.append(SECOND_HALF);
 
-        FileWriter writer = new FileWriter(index);
-        writer.write(content);
-        writer.flush();
-        writer.close();
+        try (FileWriter writer = new FileWriter(index)) {
+            writer.write(content.toString());
+            writer.flush();
+        } catch (IOException e) {
+            Nirubot.error(e.getMessage(), e);
+        }
     }
 
     
