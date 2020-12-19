4610ec2807fe0b775fd9818422cace5015abf7a7
Nils Pukropp <nils.pukropp@outlook.de>
2020-12-19 18:37:46 +0100

    updated build.sh + pom.xml

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-gradle build
+mvn clean compile assembly:single
 
-rm build/libs/nirubot
-touch build/libs/nirubot
+mkdir release
 
-echo '#!/usr/bin/java -jar' > build/libs/nirubot
-cat build/libs/nirubot-0.1-all.jar >> build/libs/nirubot
+cp target/nirubot-0.1-jar-with-dependencies.jar release/nirubot.jar
 
-chmod +x build/libs/nirubot
-
-./build/libs/nirubot
+chmod +x release/nirubot
--- update_cmdd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf /home/niru/Documents/Projects/nirubot/src/main/java/nirusu/nirucmd
-
-cp -r /home/niru/Documents/Projects/niru-cmdd/src/main/java/nirusu/nirucmd /home/niru/Documents/Projects/nirubot/src/main/java/nirusu/
